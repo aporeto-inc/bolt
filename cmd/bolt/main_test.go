697fb36ad8fc34aee2ee49bbb4f2b61944b17845
Marcus Heese <marcus@aporeto.com>
2020-06-26 12:29:17 -0700

    adjusting to be its own independent module from github.com/boltdb/bolt

--- cmd/bolt/main_test.go
+++ cmd/bolt/main_test.go
@@ -12,8 +12,8 @@ import (
 	"strconv"
 	"testing"
 
-	"github.com/boltdb/bolt"
-	"github.com/boltdb/bolt/cmd/bolt"
+	"github.com/aporeto-inc/bolt"
+	"github.com/aporeto-inc/bolt/cmd/bolt"
 )
 
 // Ensure the "info" command can print information about a database.
--- simulation_test.go
+++ simulation_test.go
@@ -7,7 +7,7 @@ import (
 	"sync"
 	"testing"
 
-	"github.com/boltdb/bolt"
+	"github.com/aporeto-inc/bolt"
 )
 
 func TestSimulate_1op_1p(t *testing.T)     { testSimulate(t, 1, 1) }
